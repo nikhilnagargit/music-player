8bae3895156a63d7b709a145f4df76a994485cfc
Nikhil Nagar <nikhilnagarcps@gmail.com>
2024-07-03 00:41:18 +0530

    feat: music player added

--- music-player/src/App.tsx
+++ music-player/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import Player from "./components/Player";
 import Sidebar from "./components/Sidebar";
 import TrackList from "./components/TrackList";
 
 function App() {
   return (
-    <div className=" bg-gradient-to-r from-gray-900 to-black h-screen p-12 flex flex-row text-white tracking-wider">
+    <div className=" bg-gradient-to-r from-gray-900 to-black h-screen p-12 flex gap-36 flex-row text-white tracking-wider">
       <Sidebar />
       <TrackList />
+      <Player />
     </div>
   );
 }
--- music-player/src/components/TrackList.tsx
+++ music-player/src/components/TrackList.tsx
@@ -3,7 +3,7 @@ import SongCard from "./SongCard";
 
 const TrackList = () => {
   return (
-    <div className="w-[35%] max-w-[550px] flex flex-col gap-10">
+    <div className="w-[35%] max-w-[500px] flex flex-col gap-10">
       <div className="flex font-semibold text-3xl gap-12">
         <h2>For You</h2>
         <h2 className="opacity-50">Top Tracks</h2>
